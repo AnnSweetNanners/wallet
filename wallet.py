75ad9f6162bfaa9cabaee8258f207627bb5c5d20
AnnSweetNanners <67086140+AnnSweetNanners@users.noreply.github.com>
2020-12-09 22:14:17 -0600

    Derive Wallet function working. Printing for all coins/variables

--- wallet.py
+++ wallet.py
@@ -4,18 +4,24 @@ from constants import *
 import os
 from dotenv import load_dotenv
 load_dotenv()
+jsonnum=[]
+
+# print(BTC, ETH, BTCTEST)
 
 mnemonic=os.getenv('mnemonic')
 # print(mnemonic)
 
-# print(BTC, ETH, BTCTEST)
 # command = f'./derive -g --mnemonic="{mnemonic}" --cols=path,address,privkey,pubkey --format=json'
 
-command = f'php derive -g --mnemonic="{mnemonic}" --cols=path,address,privkey,pubkey --format=json'
+def derive_wallet(mnemonic, coin, numderive):
+    command = f'php derive -g --mnemonic="{mnemonic}" --cols=path,address,privkey,pubkey --coin="{coin}" --numderive="{numderive}" --format=json'
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    output, err = p.communicate()
+    p_status = p.wait()
+    keys = json.loads(output)
+    return print(keys)
+    
 
-p = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-output, err = p.communicate()
-p_status = p.wait()
-
-keys = json.loads(output)
-print(keys)
+derive_wallet(mnemonic, ETH, 3)
+derive_wallet(mnemonic, BTC, 4)
+derive_wallet(mnemonic, BTCTEST,3)
